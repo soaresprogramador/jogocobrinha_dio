0589140abb12976d2a4d50f391d7242754c9efff
Paulo Soares <soaresprogramador@gmail.com>
2020-09-02 09:38:42 -0300

    Movimento

--- js/script.js
+++ js/script.js
@@ -1,10 +1,47 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
+let snake = [];
+snake [0] = {
+    x: 8 * box,
+    y: 8 * box
+}
+
+let directtion = "right";
+
 
 function criarBG(){
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
-criarBG();
+function criarCobrinha(){
+    for(i=0; i < snake.length; i++){
+        context.fillStyle = "blue";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
+function iniciarJogo(){    
+    criarBG();
+    criarCobrinha();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(directtion == "right") snakeX += box;
+    if(directtion == "left") snakeX -= box;
+    if(directtion == "up") snakeY -= box;
+    if(directtion == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarJogo, 100);
